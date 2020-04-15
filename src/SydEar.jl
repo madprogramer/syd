82c54a1083b002de3e489cb9eafec191a318948d
madprogramer <themadprogramer@gmail.com>
2020-04-15 23:44:46 +0300

    Added syd and SydEar modules

---
+++ src/SydEar.jl
@@ -0,0 +1,9 @@
+module SydEar
+
+export startListening
+
+function startListening()
+	println("How I wish, how I wish I could hear!")
+end
+
+end
--- src/syd.jl
+++ src/syd.jl
@@ -1,7 +1,22 @@
 module syd
 
+#Imports
+include("./SydEar.jl")
+
+#Import Namespaces
+using .SydEar
+
+#Basic,
+#Basically for trying basic things 
+function basic()
+	SydEar.startListening()
+end
+
+#Main Function Call
 function main()
 	
 end
 
-main()
+basic()
+# main()
+end
