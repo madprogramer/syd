c9045b9524fec6ed294e9c3f6558c964165bd77c
madprogramer <themadprogramer@gmail.com>
2020-04-17 10:52:32 +0300

    Authored Tests for sydBrain
    Testing String Matching Abilities

--- src/sydBrain.jl
+++ src/sydBrain.jl
@@ -15,5 +15,21 @@ function comprehend(words)
 	return comprehension
 end
 
+#For testing string related stuff
+function test(tests)
+	for words in tests
+		println(comprehend(words))
+	end
+end
+
+# Said
+# test(["I wanna play a game\n","Play sing emily play\n","Play sing emily play\n","Play see emily play\n",
+# 	"this is so sad\n","So ya thought ya might like to go to the show\n","Tell me is something eluding you, sunshine?\n",
+# 	"Cold iron hands clap\n","Make your name like a ghost\n"])
+
+#As Heard
+test(["i want to play game\n","lena sink amily playe\n","play seing emily plaing\n","play the emely place\n",
+	"this is so sod\n","ah suh er migdt leke to go to de show\n","down me as some thing ar luoning usasha\n",
+	"cold iron hands cleff\n","nake our name like a ghost\n"])
 
 end
